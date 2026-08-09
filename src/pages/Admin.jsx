578e4ba921e4253554180f3f746a8ea7ae4c3618
Mohammed-Zeghlache <mouhamedzeghlache7@gmail.com>
2026-08-10 00:02:31 +0200

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -622,7 +622,7 @@ function AdminLogin() {
     setLoading(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://maktaba-back.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
